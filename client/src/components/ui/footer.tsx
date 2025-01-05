d572b38d08b50961bcb49d44c607e5f8b5da7409
Michael <amichaelj1@gmail.com>
2025-01-04 20:17:03 -0500

    feat: update footer links to include Telegram and Dexscreener with correct URLs

--- client/src/components/ui/footer.tsx
+++ client/src/components/ui/footer.tsx
@@ -15,10 +15,10 @@ export const Footer = () => {
     <footer className="w-full max-w-4xl mx-auto py-6 px-4">
       <nav className="flex flex-wrap gap-4 justify-center items-center">
         <FooterLink href="https://x.com/nova_dova_dao">X</FooterLink>
-        {/* <FooterLink href="/telegram">Telegram</FooterLink> */}
+        <FooterLink href="https://t.me/novadovadao">Telegram</FooterLink>
         <FooterLink href="https://discord.gg/p5XDxgba6n">Discord</FooterLink>
         {/* <FooterLink href="/github">Github</FooterLink> */}
-        {/* <FooterLink href="/dexscreener">Dexscreener</FooterLink> */}
+        <FooterLink href="https://dexscreener.com/solana/3i8wmd25pdifbikjmklelvenjjhim3mfluabcmeofwc2">Dexscreener</FooterLink>
         <FooterLink href="https://pump.fun/coin/8HjiRvPNwFT9jpzAAsYF4rE9y576CKdTkQZXaxibpump">Pump.fun</FooterLink>
         {/* <FooterLink href="/disclaimer">Disclaimer</FooterLink> */}
         {/* <FooterLink href="/roadmap">Road Map</FooterLink> */}
